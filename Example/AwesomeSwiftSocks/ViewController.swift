06b42a341bb22a493c1e89c8efce50772a85fa62
Moray Baruh <contact@moraybaruh.com>
2016-06-22 22:31:36 +0200

    Initial commit

---
+++ Example/AwesomeSwiftSocks/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AwesomeSwiftSocks
+//
+//  Created by Moray Baruh on 06/22/2016.
+//  Copyright (c) 2016 Moray Baruh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
